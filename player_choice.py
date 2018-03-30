4c9ed8be4e7351a2143d35fd0499b2014351babd
Steve Bachmeier <steve.d.bachmeier@gmail.com>
2018-03-30 13:01:43 -0700

    Change message spacing around user inputs

--- player_choice.py
+++ player_choice.py
@@ -1,21 +1,11 @@
 def player_choice(board,turn):
 	from space_check import space_check
 	
-	# position = int(input(f"Player {turn}, you're up! Choose an available board position: "))
-	# while ((position not in range(1,10)) or (space_check(board,position) == False)):
-		# if ((position not in range(1,10)) or (isinstance(position,int) == False)):
-			# position = int(input("Must choose an integer between 1-9. Try again: "))
-			# continue
-		# elif (space_check(board,position) == False):
-			# position = int(input("That position is taken. Try again: "))
-			# continue
-	# return position
-	
 	print(f"Player {turn}, you're up!")
+	print("\n")
 	
 	while True:
 		try:
-			print("\n")
 			position = int(input("Choose an available board position: "))
 		except ValueError:
 			print("\n")
--- replay.py
+++ replay.py
@@ -1,5 +1,5 @@
 def replay():
     prompt = ""
     while ((prompt.upper() != "Y") and (prompt.upper() != "N")):
-        prompt = input("Would you like to play again (y/n)?")
+        prompt = input("Would you like to play again (y/n)? ")
     return prompt.upper() == "Y"
